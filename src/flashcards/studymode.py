4f9e31d24331b1986ada71e32ecf3cf11857cd24
Cpun077 <cadenpun@gmail.com>
2023-10-20 15:17:25 -0700

    add studymode generation

---
+++ src/flashcards/studymode.py
@@ -0,0 +1,49 @@
+import random
+
+def createQuiz(deck):
+    print(deck)
+    terms = []
+    definitions = []
+    newquiz = []
+
+    # Insert all terms and their respective correct definitions into the quiz
+    for i in range(len(deck['flashcards'])):
+        newquiz.append({
+            'question': deck['flashcards'][i]['t'],
+            'answeroptions': [{'option': deck['flashcards'][i]['d'], 'isCorrect': True}]
+        })
+        terms.append(deck['flashcards'][i]['t'])
+        definitions.append(deck['flashcards'][i]['d'])
+
+    # Go through all quiz questions
+    for i in range(len(newquiz)):
+        # Insert other random definitions (up to 3 and at least 1) for each term
+        if len(definitions) < 4:
+            # Go through all definitions
+            for j in range(len(definitions)):
+                # If not the correct definition, add the definition to options
+                if definitions[j] != newquiz[i]['answeroptions'][0]['option']:
+                    newquiz[i]['answeroptions'].append({'option': definitions[j], 'isCorrect': False})
+        else:
+            # Add a limit of 3 definitions
+            for j in range(3):
+                matches = True
+                randef = 'ERROR'
+                # Get a random definition until it's not already in options
+                while matches:
+                    randef = random.choice(definitions)
+                    # Iterate through options
+                    for x in range(len(newquiz[i]['answeroptions'])):
+                        duplicate = False
+                        for y in range(len(newquiz[i]['answeroptions'])):
+                            if randef == newquiz[i]['answeroptions'][y]['option']:
+                                duplicate = True
+                        if not duplicate:
+                            matches = False
+                newquiz[i]['answeroptions'].append({'option': randef, 'isCorrect': False})
+        random.shuffle(newquiz[i]['answeroptions'])
+
+    # Shuffle quiz questions and return quiz
+    random.shuffle(newquiz)
+    print('finished creating quiz:', newquiz)
+    return newquiz
